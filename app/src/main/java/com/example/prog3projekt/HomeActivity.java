e6bb4695a8dd3d08c5614bada82632f9b98e53e4
TomSattler <tomsattler11@gmail.com>
2022-02-11 14:45:59 +0100

    Merge von beiden Branches funktioniert. !! Intent von Main zur Home funktioniert noch nicht werde ich gleich updaten.

---
+++ app/src/main/java/com/example/prog3projekt/HomeActivity.java
@@ -0,0 +1,94 @@
+package com.example.prog3projekt;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.Series;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+    Button heute;
+    GraphView graphView;
+    List <Note> allNotes;
+    NoteViewModel noteViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        heute = findViewById(R.id.heuteBtn);
+        graphView = findViewById(R.id.graphView);
+        Date t = DataTimeHilfsKlasse.getTodaysDateAsDate();
+        DataTimeHilfsKlasse.getDatesDayAsInt(t);
+        graphViewInitialisieren(30, 100);
+
+        //Button zur "Erstellung" eines neuen Tages"
+        heute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //OnClickListener auf der Statistik, zur ausführlicheren Statistik in der Statistik Activity
+        graphView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, statistikActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    // https://github.com/jjoe64/GraphView/wiki/Style-options Dokumentation von der GraphView
+    void graphViewInitialisieren(int maxX, int maxY){
+        graphView = findViewById(R.id.graphView);
+        noteViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            //onChanged, ebenso wie bei der MainActivity, wird der Graph verändert wenn sich das DataSet verändert.
+            public void onChanged(@Nullable List<Note> notes) {
+                allNotes = notes;
+                DataPoint[] s = new DataPoint[allNotes.size()];
+                for (int i = 0; i<allNotes.size();i++) {
+                    s[i] = new DataPoint(allNotes.get(i).getSchwierigkeit(), i);
+                }
+                Series<DataPoint> series = new LineGraphSeries<>(s);
+                setUpGraphView(graphView, maxX, maxY);
+                updateGraphView(graphView, series);
+            }
+        });
+
+    }
+    //Hilfsmethode um den Graphen zu zeichnen
+    private void setUpGraphView(GraphView graphView, int maxX, int maxY){
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setYAxisBoundsManual(true);
+        graphView.getViewport().setMinY(0);
+        graphView.getViewport().setMaxY(maxY);
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setMaxX(maxX);
+        graphView.setTitleColor(R.color.main_color);
+        graphView.setTitleTextSize(18);
+    }
+    //Hilsmethoden um bei upgedatetem DataSet den Graphen neu zu zeichnen
+    private void updateGraphView(GraphView graphView, Series<DataPoint> s){
+        graphView.addSeries(s);
+    }
+}
